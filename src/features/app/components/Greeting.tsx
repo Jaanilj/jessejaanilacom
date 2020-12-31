1ca31f71d186b9ce610055b4f20a97fd9d3065d5
jaanilj <jesse.jaanila@relexsolutions.com>
2020-12-31 13:00:53 +0200

    Update webpage message

--- src/features/app/components/Greeting.tsx
+++ src/features/app/components/Greeting.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import styles from './Greeting.module.less'
 
+const instagramUrl = 'https://www.instagram.com/jaanilajesse/'
+
 export default function Greeting() {
   return (
     <>
       <h1>Welcome to my webpage!</h1>
-      <span className={styles.message}>
-        Please play a game of chess against me while your here.
-      </span>
+      <p className={styles.message}>
+        I'm a software developer living in Helsinki. I'm interest in the
+        outdoors, breakfast, lattès, and Crossfit. You can see what i'm doing
+        with my life via <a href={instagramUrl}>@jaanilajesse</a>.
+      </p>
     </>
   )
 }
