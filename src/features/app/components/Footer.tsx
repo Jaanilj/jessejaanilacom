2de0ce84540c7df77556e8ac22861c7ca3d0dbce
jaanilj <jesse.jaanila@relexsolutions.com>
2021-04-10 14:29:44 +0300

    Add footer

---
+++ src/features/app/components/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Footer.module.less'
+
+export default function Footer(): JSX.Element {
+  return (
+    <footer className={styles.footer}>Copyright @ 2021 Jesse Jaanila</footer>
+  )
+}
--- src/features/app/containers/App.tsx
+++ src/features/app/containers/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import About from '../components/About'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import styles from './App.module.less'
 
@@ -8,6 +9,7 @@ export default function App(): JSX.Element {
     <div className={styles.container}>
       <Header />
       <About />
+      <Footer />
     </div>
   )
 }
